090bac424371e443071622354be0d9d653f4fcfe
Vladimir <vladimir.buskin@gmail.com>
2022-08-24 22:09:02 -0700

    .

--- sprint7/i.py
+++ sprint7/i.py
@@ -9,16 +9,46 @@ for i in range(n):
 
 for r in range(n-1,-1,-1): # 1,0
   for c in range(0, m):
-    lft = 0
-    bot = 0
-    if r+1 < n:
-      bot = d[r+1][c]
-    if c-1 >= 0:
-      lft = d[r][c-1]
+    bot = d[r+1][c] if r+1 < n else 0
+    lft = d[r][c-1] if c-1 >= 0 else 0
 
     d[r][c] = max(bot,lft) + (1 if ar[r][c]=='1' else 0)
 
+path = []
+
+r = 0
+c = m-1
+
+
+while (r < n-1 or c > 0):
+
+  if r == n-1:
+    c-=1
+    path.append("R")
+    # print('finish R', r, c)
+    continue
+
+  if c == 0:
+    r+=1
+    path.append("U")
+    # print('finish U')
+    continue
+
+  bot = d[r+1][c]
+  lft = d[r][c-1]
+  if lft >= bot:
+    path.append("R")
+    c-=1
+    # print('move R')
+  else:
+    path.append("U")
+    r+=1
+    # print('move U')
+
 print(d[0][m-1])
+path.reverse()
+print("".join(path))
+
 
 # print(ar)
 # print(d)
